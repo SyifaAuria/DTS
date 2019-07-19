b2240140a0bc035e23445bb0c9b22309551723b2
SyifaAuria <52481548+SyifaAuria@users.noreply.github.com>
2019-07-19 17:17:04 +0800

    P12B

---
+++ Pertwmuan12b/app/src/main/java/com/example/pertwmuan12b/konfigurasi.java
@@ -0,0 +1,22 @@
+package com.example.pertwmuan12b;
+
+public class konfigurasi {
+    public static  final String URL_ADD="http://192.168.43.225/pegawai/tambahpgw.php";
+    public static  final String URL_GET_ALL="http://192.168.43.225/pegawai/tampilsemuapgw.php";
+    public static  final String URL_GET_EMP="http://192.168.43.225/pegawai/tampilpgw.php";
+    public static  final String URK_UPDATE_EMP="http://192.168.43.225/pegawai/updatepgw.php";
+    public static  final String URL_DELETE_EMP="http://192.168.43.225/pegawai/hapus.php?idm";
+
+    public static final String KEY_EMP_ID = "id";
+    public static final String KEY_EMP_NAMA = "name";
+    public static final String KEY_EMP_POSISI = "position";
+    public static final String KEY_EMP_GAJIH = "salary";
+
+    public static final String TAG_JSON_ARRAY = "result";
+    public static final String TAG_ID = "id";
+    public static final String TAG_NAMA = "name";
+    public static final String TAG_POSISI = "position";
+    public static final String TAG_GAJIH = "salary";
+
+    public static final String EMP_ID = "emp_id";
+}
